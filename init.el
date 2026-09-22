632b2466b15a6bac64a999f2413a082b0621560b
antoine.renciot@gmail.com <antoine.renciot@gmail.com>
2026-09-22 22:06:39 +0200

    chore : init

---
+++ init.el
@@ -0,0 +1,35 @@
+
+;; empty path by default
+(setq insert-default-directory nil)
+
+
+(load-theme 'wombat t)  ;; t = no prompt
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(wombat))
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(package-selected-packages '(web-mode))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'downcase-region 'disabled nil)
+
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+
+(set-face-attribute 'default nil :height 180)
+
+(global-display-line-numbers-mode 1)
